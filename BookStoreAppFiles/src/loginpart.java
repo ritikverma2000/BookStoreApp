e35d57eb98fb57c8be3e9a17cf9c1972c70ef101
ritikverma2000 <varmaritik04@gmail.com>
2020-12-29 19:07:37 +0530

    minor changes

--- BookStoreAppFiles/src/loginpart.java
+++ BookStoreAppFiles/src/loginpart.java
@@ -18,28 +18,28 @@ public class loginpart extends BookShop {
                 f1.setSize(200, 200);
                 Label n = new Label("User Name:", 1);
                 Label p = new Label("Password:", 1);
-                this.name = new TextField(40);
-                this.pass = new TextField(40);
-                this.showPassword = new Checkbox("Show Password");
-                this.loginButton = new Button("SIGN-IN");
-                this.resetButton = new Button("RESET");
-                this.loginButton.addActionListener(this);
-                this.resetButton.addActionListener(this);
-                this.showPassword.addItemListener(this);
+                name = new TextField(40);
+                pass = new TextField(40);
+                showPassword = new Checkbox("Show Password");
+                loginButton = new Button("SIGN-IN");
+                resetButton = new Button("RESET");
+                loginButton.addActionListener(this);
+                resetButton.addActionListener(this);
+                showPassword.addItemListener(this);
                 f1.add(n);
-                f1.add(this.name);
+                f1.add(name);
                 f1.add(p);
-                f1.add(this.pass);
-                f1.add(this.showPassword);
-                f1.add(this.loginButton);
-                f1.add(this.resetButton);
+                f1.add(pass);
+                f1.add(showPassword);
+                f1.add(loginButton);
+                f1.add(resetButton);
                 n.setBounds(50, 150, 100, 30);
                 p.setBounds(50, 220, 100, 30);
-                this.name.setBounds(150, 150, 150, 30);
-                this.pass.setBounds(150, 220, 150, 30);
-                this.showPassword.setBounds(150, 260, 150, 30);
-                this.loginButton.setBounds(60, 300, 100, 30);
-                this.resetButton.setBounds(210, 300, 100, 30);
+                name.setBounds(150, 150, 150, 30);
+                pass.setBounds(150, 220, 150, 30);
+                showPassword.setBounds(150, 260, 150, 30);
+                loginButton.setBounds(60, 300, 100, 30);
+                resetButton.setBounds(210, 300, 100, 30);
                 f1.setBounds(10, 10, 370, 600);
                 f1.setLayout((LayoutManager) null);
                 f1.setVisible(true);
@@ -52,16 +52,16 @@ public class loginpart extends BookShop {
             }
 
             public void actionPerformed(ActionEvent e) {
-                String username = this.name.getText();
-                String password = this.pass.getText();
-                if (e.getSource() == this.loginButton) {
+                String username = name.getText();
+                String password = pass.getText();
+                if (e.getSource() ==loginButton) {
                     System.out.println(username);
                     System.out.println(password);
                 }
 
-                if (e.getSource() == this.resetButton) {
-                    this.name.setText("");
-                    this.pass.setText("");
+                if (e.getSource() == resetButton) {
+                    name.setText("");
+                    pass.setText("");
                 }
 
             }
